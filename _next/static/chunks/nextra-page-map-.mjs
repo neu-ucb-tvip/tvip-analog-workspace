25ddd8e7696ea28b0cdc3ddb94307bec64416bb6
daniellovell <daniellovell@users.noreply.github.com>
2025-08-17 05:37:42 +0000

    deploy: ff487bd5f03e87dd6a37d62484af79b3b4510c69

---
+++ _next/static/chunks/nextra-page-map-.mjs
@@ -0,0 +1,75 @@
+import meta from "../../../pages/_meta.ts";
+export const pageMap = [{
+  data: meta
+}, {
+  name: "Analog_Layout_Guide",
+  route: "/Analog_Layout_Guide",
+  frontMatter: {
+    "title": "Analog Layout Guide",
+    "authors": [{
+      "name": "Daniel Lovell",
+      "email": "dlovell98@berkeley.edu",
+      "affiliation": "University of California, Berkeley"
+    }, {
+      "name": "Nikhil Jain",
+      "email": "njain_cal@berkeley.edu",
+      "affiliation": "University of California, Berkeley"
+    }],
+    "lastUpdated": new Date(1749168000000)
+  }
+}, {
+  name: "Git_Guide",
+  route: "/Git_Guide",
+  frontMatter: {
+    "title": "Git Guide",
+    "authors": [{
+      "name": "Daniel Lovell",
+      "email": "dlovell98@berkeley.edu",
+      "affiliation": "University of California, Berkeley"
+    }],
+    "lastUpdated": new Date(1755129600000)
+  }
+}, {
+  name: "impedance-matching",
+  route: "/impedance-matching",
+  frontMatter: {
+    "title": "Impedance Matching",
+    "authors": [{
+      "name": "Nikhil Jain",
+      "email": "njain_cal@berkeley.edu",
+      "affiliation": "University of California, Berkeley"
+    }],
+    "lastUpdated": new Date(1749513600000)
+  }
+}, {
+  name: "index",
+  route: "/",
+  frontMatter: {
+    "title": "TVIP Analog Workspace",
+    "description": "Analog Design Workspace for Sky130 PDK"
+  }
+}, {
+  name: "Power_Grids",
+  route: "/Power_Grids",
+  frontMatter: {
+    "title": "Power Grids in VLSI",
+    "authors": [{
+      "name": "Daniel Lovell",
+      "email": "dlovell98@berkeley.edu",
+      "affiliation": "University of California, Berkeley"
+    }],
+    "lastUpdated": new Date(1749168000000)
+  }
+}, {
+  name: "Running_DRC",
+  route: "/Running_DRC",
+  frontMatter: {
+    "title": "Running DRC",
+    "authors": [{
+      "name": "Daniel Lovell",
+      "email": "dlovell98@berkeley.edu",
+      "affiliation": "University of California, Berkeley"
+    }],
+    "lastUpdated": new Date(1751760000000)
+  }
+}];
